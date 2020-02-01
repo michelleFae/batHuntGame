f49f0c3f1093c1697514e50b775b4bb66c33af0d
michelleFae <39632954+michelleFae@users.noreply.github.com>
2020-02-01 14:00:53 -0800

    uploaded scripts and scripts meta

---
+++ Scripts/Chest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+
+#region gameObject_variables
+	[SerializeField]
+	[Tooltip("HealthPack")]
+	private GameObject healthPack;
+
+	#endregion
+
+	#region helper_functions
+	IEnumerator DeleteChest(){
+		yield return new WaitForSeconds(0.3f);
+		Instantiate(healthPack, transform.position, transform.rotation);
+		Destroy(gameObject);
+		Debug.Log("bye chest");
+	}
+
+	public void Interact() {
+		StartCoroutine(DeleteChest());
+
+	}
+	#endregion
+}
---
+++ Scripts/LineofSight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineofSight : MonoBehaviour
+{
+	// called wjen something enters the trigger colider
+    private void OnTriggerEnter2D(Collider2D coll){
+    	if (coll.CompareTag("Player"))
+    	{
+    		GetComponentInParent<Enemy>().player = coll.transform;
+    		Debug.Log("run at player");
+    	}
+
+    }
+
+    
+}
